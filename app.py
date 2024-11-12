83fdde8ebe3f93a1a0c35c1457e552ddb884eb55
Emma Martini <emma.martini@hotmail.se>
2024-11-12 21:30:33 +0100

    This should build the app

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ from get_data import getHourlyWeatherData
 import os
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     API_KEY = os.getenv('API_KEY')
     return render_template('index.html',data=getHourlyWeatherData(latitude=59.3293,longitude=18.0686,API_KEY=API_KEY,units='metric'))
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
